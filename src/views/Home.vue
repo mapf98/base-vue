8e59db54a15cab73e850259c58e91a7abc897b4e
Miguel Fraga <mapena.16@est.ucab.edu.ve>
2020-04-05 12:19:28 -0400

    Se agrego vuetify y se creo un componente para prueba

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container fluid>
+    <v-row>
+      <v-col class="d-flex justify-center">
+        <v-btn class="ma-2" tile outlined color="success" x-large>
+          GET Banks
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="d-flex flex-wrap justify-center">
+        <v-card color="#385F73" dark min-width="500" class="ma-5">
+          <v-card-title class="headline text-center">Bank name</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+@Component({})
+export default class Home extends Vue {}
 </script>
+
+<style lang="scss"></style>
